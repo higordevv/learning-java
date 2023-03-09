c56d90a3bc3d84b36636325f946086397862988c
Higor <98431065+higordevv@users.noreply.github.com>
2023-03-09 17:30:03 +0000

    Creater folder: Exercises

---
+++ Exercises/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero1, numero2,
+                soma, subtracao,
+                multiplicacao,
+                divisao,resto;
+
+        System.out.println("==========[ Calculadora Foda ]==========");
+        System.out.print("Digite o primeiro numero: ");
+        numero1 = sc.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        numero2 = sc.nextInt();
+
+        soma = numero1 + numero2;
+        subtracao = numero1 - numero2;
+        multiplicacao = numero1 * numero2;
+        divisao = numero1 / numero2;
+        resto = numero1 % numero2;
+
+        // Output
+        System.out.println("A soma dos números é: " + soma);
+        System.out.println("A subtração dos números é: " + subtracao);
+        System.out.println("A multiplicação dos números é: " + multiplicacao);
+        System.out.println("A divisão dos números é: " + divisao);
+        System.out.println("O resto da divisão dos números é: " + resto);
+
+        sc.close();
+    }
+}
